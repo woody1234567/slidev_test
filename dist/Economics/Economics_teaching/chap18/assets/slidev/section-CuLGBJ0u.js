85e73dbfd8984a72c2152f9cb3c8452340e42da8
woody <johannes8873@gmail.com>
2026-02-10 23:47:09 +0800

    feat:do not commit the dist

--- dist/Economics/Economics_teaching/chap18/assets/slidev/section-CuLGBJ0u.js
+++
@@ -1,1 +0,0 @@
-import{o as t,e as s,g as o,i as a}from"../modules/vue-DuThAxmh.js";import{u as r}from"./context-BFhGL0qc.js";const c={class:"slidev-layout section w-full h-full grid"},l={class:"my-auto text-center"},u={__name:"section",setup(n){return r(),(e,i)=>(t(),s("div",c,[o("div",l,[a(e.$slots,"default")])]))}};export{u as _};
--- dist/Economics/Economics_teaching/chap7/assets/slidev/section-DYMINay8.js
+++
@@ -1,1 +0,0 @@
-import{o as t,e as s,g as o,i as a}from"../modules/vue-D0_OrBWU.js";import{u as r}from"./context-B-e5o8ue.js";const c={class:"slidev-layout section w-full h-full grid"},l={class:"my-auto text-center"},u={__name:"section",setup(n){return r(),(e,i)=>(t(),s("div",c,[o("div",l,[a(e.$slots,"default")])]))}};export{u as _};
